beba6ef5176f6adf9179cb2938cbaee65ae6b118
Chris Hayen <chayen@gmail.com>
2025-05-24 17:26:25 -0400

    works with generic types

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,47 @@
 use std::{
     collections::BTreeMap,
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
 };
 
+use bincode::{self, Decode, Encode};
+use serde::{Serialize, de::DeserializeOwned};
+
 pub struct Store {
-    memtable: Arc<Mutex<BTreeMap<String, String>>>,
+    memtable: Arc<RwLock<BTreeMap<String, Vec<u8>>>>,
+    pub memtable_size_limit: usize,
+    bincode_config: bincode::config::Configuration,
 }
 
 impl Store {
     pub fn new() -> Self {
         Self {
-            memtable: Arc::new(Mutex::new(BTreeMap::new())),
+            memtable: Arc::new(RwLock::new(BTreeMap::new())),
+            bincode_config: bincode::config::standard().with_little_endian(),
+            memtable_size_limit: 10,
         }
     }
 
-    pub fn set(&self, key: &str, value: String) -> Result<(), Box<dyn std::error::Error>> {
-        let mut memtable = self.memtable.lock().unwrap();
-        memtable.insert(key.to_string(), value.clone());
+    pub fn set<V: Serialize + Encode>(
+        &self,
+        key: &str,
+        value: V,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let serialized_value = bincode::encode_to_vec(&value, self.bincode_config)?;
+        let mut memtable = self.memtable.write().unwrap();
+        memtable.insert(key.to_string(), serialized_value);
 
         Ok(())
     }
 
-    pub fn get(&self, key: String) -> Result<Option<String>, Box<dyn std::error::Error>> {
-        let memtable = self.memtable.lock().unwrap();
-        if let Some(value) = memtable.get(&key) {
-            return Ok(Some(value.clone()));
+    pub fn get<V: DeserializeOwned + Decode<()>>(
+        &self,
+        key: String,
+    ) -> Result<Option<V>, Box<dyn std::error::Error>> {
+        let memtable = self.memtable.read().unwrap();
+        if let Some(serialized_value) = memtable.get(&key) {
+            let (value, _): (V, _) =
+                bincode::decode_from_slice(serialized_value, self.bincode_config)?;
+            return Ok(Some(value));
         }
 
         Ok(None)
@@ -41,5 +58,9 @@ mod tests {
         store.set("key", "value".to_string()).unwrap();
         let value = store.get("key".to_string()).unwrap();
         assert_eq!(value, Some("value".to_string()));
+
+        store.set("number", 10).unwrap();
+        let value = store.get("number".to_string()).unwrap();
+        assert_eq!(value, Some(10));
     }
 }
